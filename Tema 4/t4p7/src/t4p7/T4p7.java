4bd62e54cdaef85ad42caf9934cb3f039c74e20e
pepiperezoso <alejandro.perez@ikasle.egibide.org>
2019-11-11 13:14:39 +0100

    Tema 4  Ej 7 terminado

--- Tema 4/t4p7/src/t4p7/T4p7.java
+++ Tema 4/t4p7/src/t4p7/T4p7.java
@@ -18,14 +18,78 @@ public class T4p7 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String devolver = "";
         double importe = 0;
-        double[] cambio= {0.01,0.02,0.05,0.1,0.2,0.5,1,2,5,10,20,50,100,200,500};
+        double[][] cambio= {
+            {500,200,100,50,20,10,5,2,1,0.5,0.2,0.1,0.05,0.02,0.01}, // [0][i]
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}                          // [1][i]
+        };
         double total = Double.parseDouble(JOptionPane.showInputDialog("Introduce el total a pagar"));
+        double vueltas = 0;
         do{
        importe = Double.parseDouble(JOptionPane.showInputDialog("Introduce el dinero"));
         if (importe<total)
             JOptionPane.showMessageDialog(null, "No puedes dar menos de lo que tienes que pagar!");
-    
+        
         }while(importe<total);
+        vueltas = importe - total;
+        do{
+       if (vueltas>=500){
+           cambio[1][0]+=1;
+           vueltas = vueltas - 500;
+       }else     if (vueltas>=200){
+           cambio[1][1]+=1;
+           vueltas = vueltas - 200;
+       }else     if (vueltas>=100){
+           cambio[1][2]+=1;
+           vueltas = vueltas - 100;
+        }else     if (vueltas>=50){
+           cambio[1][3]+=1;
+           vueltas = vueltas - 50;
+        }else     if (vueltas>=20){
+           cambio[1][4]+=1;
+           vueltas = vueltas - 20; 
+        }else     if (vueltas>=10){
+           cambio[1][5]+=1;
+           vueltas = vueltas - 10;  
+        }else     if (vueltas>=5){
+           cambio[1][6]+=1;
+           vueltas = vueltas - 5;
+        }else     if (vueltas>=2){
+           cambio[1][7]+=1;
+           vueltas = vueltas - 2;
+        }else     if (vueltas>=1){
+           cambio[1][8]+=1;
+           vueltas = vueltas - 1;
+        }else     if (vueltas>=0.5){
+           cambio[1][9]+=1;
+           vueltas = vueltas - 0.5;
+         }else     if (vueltas>=0.2){
+           cambio[1][10]+=1;
+           vueltas = vueltas - 0.2;
+         }else     if (vueltas>=0.1){
+           cambio[1][11]+=1;
+           vueltas = vueltas - 0.1;
+         }else     if (vueltas>=0.05){
+           cambio[1][12]+=1;
+           vueltas = vueltas - 0.05;
+         }else     if (vueltas>=0.02){
+           cambio[1][13]+=1;
+           vueltas = vueltas - 0.02;
+         }else     if (vueltas>=0.01){
+           cambio[1][14]+=1;
+           vueltas = vueltas - 0.01;
+         }
+           
+       
     
+    
+    }while(vueltas != 0);
+        for (int i = 0; i < 15; i++) {
+           devolver +=  "Billetes de "+ cambio[0][i]+" son "+cambio[1][i]+"\n";
+        }
+        JOptionPane.showMessageDialog(null, devolver);
+        
+    
+}
 }
